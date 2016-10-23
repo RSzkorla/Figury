834095bfabe1ffd2d3b9c2f6b8df55fc91a06997
Radek <r.szkorla@gmail.com>
2016-10-23 16:27:47 +0200

    dodanie kolejnych plików

--- Figury/Program.cs
+++ Figury/Program.cs
@@ -8,14 +8,23 @@ namespace Figury
 {
   class Program
   {
-    static private int dokladnosc {get;set;}
+    static private int dokladnosc = 2;
     static private List<Bryla> figury = new List<Bryla>();
     static void DodajFiguryDoListy()
     {
       figury.Add(new Kula());
+      figury.Add(new Ostroslup());
     }
     static void Main(string[] args)
     {
+      DodajFiguryDoListy();
+      foreach(Bryla i in figury)
+      {
+        Console.WriteLine(i.Nazwa());
+        i.WprowadzDane();
+        Console.WriteLine($"Pole: {i.LiczPole(dokladnosc)}, Objętość: {i.LiczObjetosc(dokladnosc)}");
+
+      }
     }
   }
 }
