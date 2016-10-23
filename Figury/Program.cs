b4df9ce4ad4ae329a732b018f7c225277b76e10a
Radek <r.szkorla@gmail.com>
2016-10-23 15:17:39 +0200

    dodanie plików

---
+++ Figury/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Figury
+{
+  class Program
+  {
+    static private int dokladnosc {get;set;}
+    static private List<Bryla> figury = new List<Bryla>();
+    static void DodajFiguryDoListy()
+    {
+      figury.Add(new Kula());
+    }
+    static void Main(string[] args)
+    {
+    }
+  }
+}
